fa531018ea4218c2d6af5031d3205778ef2b29be
max-schofield <max.schofield@tridentsystems.co.nz>
2015-10-14 10:44:00 +1300

    delete repeated packages

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -6,9 +6,8 @@ packages <- c(
     "shapefiles", "foreign", "sp", "grid", "lattice", "rgeos", "RColorBrewer",
     "maptools", "RPostgreSQL", "knitr", "rjson", "pander", "ggplot2", "dplyr",
     "tables", "data.table", "tidyr", "gridExtra", "rjags", "R2jags", "reshape2", 
-    "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'lubridate', 'reshape2', 'gridExtra', 
-    'scales', 'rgdal', 'maptools', 'ggmap', 'Cairo', 'maps', 'tables', 'Matching', 
-    'BenfordTests', 'genoud'
+    "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'lubridate', 'scales', 'rgdal', 
+    'ggmap', 'Cairo', 'maps', 'Matching', 'BenfordTests', 'genoud'
 )
 
 for (p in packages) {
