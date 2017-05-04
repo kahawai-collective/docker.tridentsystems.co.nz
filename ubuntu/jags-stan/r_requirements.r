a6d58bf29843a27932ca139db0c6bc2745e27aec
Yvan Richard <yvan@dragonfly.co.nz>
2017-05-04 15:10:23 +1200

    Adding loo to jags-stan image

--- ubuntu/jags-stan/r_requirements.r
+++ ubuntu/jags-stan/r_requirements.r
@@ -2,7 +2,7 @@ old <- getOption("defaultPackages"); r <- getOption("repos")
 r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
 options(defaultPackages = c(old, "MASS"), repos = r)
 
-packages <- c("rstan", "runjags", "rbokeh", "viridis")
+packages <- c("rstan", "runjags", "rbokeh", "viridis", "loo")
 packages <- setdiff(packages, library()$results[,"Package"])
 
 for (p in packages) {
