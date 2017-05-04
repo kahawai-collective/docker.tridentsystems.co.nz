3a58613cd2b6398d40ca6971cf3c6b3e85560b17
Yvan Richard <yvan@dragonfly.co.nz>
2017-05-04 14:41:14 +1200

    fix building of jags-stan

--- ubuntu/jags-stan/r_requirements.r
+++ ubuntu/jags-stan/r_requirements.r
@@ -3,7 +3,7 @@ r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
 options(defaultPackages = c(old, "MASS"), repos = r)
 
 packages <- c("rstan", "runjags", "rbokeh", "viridis")
-packages <- setdiff(packages, library()$$results[,"Package"])
+packages <- setdiff(packages, library()$results[,"Package"])
 
 for (p in packages) {
     if (!require(p, character.only=TRUE)) {
