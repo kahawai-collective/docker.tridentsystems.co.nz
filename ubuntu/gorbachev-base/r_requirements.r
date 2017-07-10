00fc87d668ff1f211bad34f42f1177c58fd94ee2
Max Schofield <max.schofield@tridentsystems.co.nz>
2017-07-10 14:28:00 +1200

    add gam

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -5,11 +5,11 @@ options(defaultPackages = c(old, "MASS"), repos = r)
 packages <- c(
     "shapefiles", "foreign", "sp", "grid", "lattice", "rgeos", "RColorBrewer",
     "maptools", "RPostgreSQL", "knitr", "rjson", "pander", "ggplot2", "dplyr",
-    "tables", "data.table", "tidyr", "gridExtra", "rjags", "R2jags", "reshape2", 
-    "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'lubridate', 'scales', 'rgdal', 
-    'ggmap', 'Cairo', 'maps', 'Matching', 'BenfordTests', 'genoud', 'tools', 
-    'utils', 'rgenoud', 'broom','purrr', 'cowplot', 'MASS', 'gridBase', 'pryr', 
-    'beanplot', 'mapdata', 'rpart', 'caret', 'openxlsx', 'readxl', 'GGally'
+    "tables", "data.table", "tidyr", "gridExtra", "rjags", "R2jags", "reshape2",
+    "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'lubridate', 'scales', 'rgdal',
+    'ggmap', 'Cairo', 'maps', 'Matching', 'BenfordTests', 'genoud', 'tools',
+    'utils', 'rgenoud', 'broom','purrr', 'cowplot', 'MASS', 'gridBase', 'pryr',
+    'beanplot', 'mapdata', 'rpart', 'caret', 'openxlsx', 'readxl', 'GGally', 'gam'
 )
 
 for (p in packages) {
