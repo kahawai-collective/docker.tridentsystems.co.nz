34d3fff6d704ab3d3c4d4850ae6ea2d739e7f0bf
Richard Mansfield <richard@dragonfly.co.nz>
2017-11-10 12:07:26 +1300

    Remove uninstallable packages from the list

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -2,13 +2,15 @@ old <- getOption("defaultPackages"); r <- getOption("repos")
 r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
 options(defaultPackages = c(old, "MASS"), repos = r, warn=2)
 
+# packages ‘grid’, ‘genoud’, ‘tools’, ‘utils’ are not available (for R version 3.4.2)
+
 packages <- c(
-    "shapefiles", "foreign", "sp", "grid", "lattice", "rgeos", "RColorBrewer",
+    "shapefiles", "foreign", "sp", "lattice", "rgeos", "RColorBrewer",
     "maptools", "RPostgreSQL", "knitr", "rjson", "pander", "tidyverse",
     "tables", "data.table", "gridExtra", "rjags", "R2jags", "reshape2",
     "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'scales', 'rgdal',
-    'ggmap', 'Cairo', 'maps', 'Matching', 'BenfordTests', 'genoud', 'tools',
-    'utils', 'rgenoud', 'broom', 'cowplot', 'MASS', 'gridBase', 'pryr',
+    'ggmap', 'Cairo', 'maps', 'Matching', 'BenfordTests', 'rgenoud',
+    'broom', 'cowplot', 'MASS', 'gridBase', 'pryr',
     'beanplot', 'mapdata', 'rpart', 'caret', 'openxlsx', 'readxl', 'GGally', 'gam',
     'mgcv', 'geosphere', 'dbplyr', 'timeline', 'ggforce'
 )
