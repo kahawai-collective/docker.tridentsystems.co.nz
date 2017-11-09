22190c79ea47a7fb8f032ad6420be297a4d54b33
Richard Mansfield <richard@dragonfly.co.nz>
2017-11-10 11:59:32 +1300

    Try treating warnings as errors

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -1,6 +1,6 @@
 old <- getOption("defaultPackages"); r <- getOption("repos")
 r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
-options(defaultPackages = c(old, "MASS"), repos = r)
+options(defaultPackages = c(old, "MASS"), repos = r, warn=2)
 
 packages <- c(
     "shapefiles", "foreign", "sp", "grid", "lattice", "rgeos", "RColorBrewer",
@@ -15,7 +15,13 @@ packages <- c(
 
 update.packages(ask=F)
 
-pkgs2install <- setdiff(packages, library()$results[, 'Package'])
+existing <- tryCatch({
+    library()$results[, 'Package']
+}, error = function (x) {
+    c()
+})
+
+pkgs2install <- setdiff(packages, existing)
 install.packages(pkgs2install)
 
 # INLA has its own repo...
