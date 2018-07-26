ffa59c8958a23afa30b3064c395cd7a8925cc465
Richard Mansfield <richard@dragonfly.co.nz>
2018-07-26 16:42:43 +1200

    Install more R libs in fonz docker

---
+++ ubuntu/fonz/r_requirements.r
@@ -0,0 +1,16 @@
+old <- getOption("defaultPackages"); r <- getOption("repos")
+r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
+options(defaultPackages = c(old, "MASS"), repos = r, warn=2)
+
+packages <- c("cluster", "gtools")
+
+update.packages(ask=F)
+
+existing <- tryCatch({
+    library()$results[, 'Package']
+}, error = function (x) {
+    c()
+})
+
+pkgs2install <- setdiff(packages, existing)
+install.packages(pkgs2install)
