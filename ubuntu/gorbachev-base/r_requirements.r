bc0430490c12dd880c8d6a33700e89c00b9c11d5
Richard Mansfield <richard@dragonfly.co.nz>
2018-10-25 15:46:14 +1300

    Pin rgeos to 0.3 version

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -5,7 +5,7 @@ options(defaultPackages = c(old, "MASS"), repos = r, warn=2)
 # packages ‘grid’, ‘genoud’, ‘tools’, ‘utils’ are not available (for R version 3.4.2)
 
 packages <- c(
-    "shapefiles", "foreign", "sp", "lattice", "rgeos", "RColorBrewer",
+    "shapefiles", "foreign", "sp", "lattice", "RColorBrewer",
     "maptools", "RPostgreSQL", "knitr", "rjson", "pander", "tidyverse",
     "tables", "data.table", "gridExtra", "rjags", "R2jags", "reshape2",
     "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'scales',
@@ -27,3 +27,6 @@ existing <- tryCatch({
 
 pkgs2install <- setdiff(packages, existing)
 install.packages(pkgs2install)
+
+old_rgeos <- "https://cran.r-project.org/src/contrib/Archive/rgeos/rgeos_0.3-28.tar.gz"
+install.packages(old_rgeos, repos=NULL, type="source")
