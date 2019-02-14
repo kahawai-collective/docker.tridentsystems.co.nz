64713a263b74fea1621eb0b9e33b42b3ef0e0359
markschambers <39928886+markschambers@users.noreply.github.com>
2019-02-15 10:55:14 +1300

    Add packages DHARMa and MuMIn

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -15,7 +15,7 @@ packages <- c(
     'mgcv', 'geosphere', 'dbplyr', 'timeline', 'ggforce', 'CheckDigit', 'tinytex',
     'kableExtra', 'fuzzyjoin','lunar','gamlss','glmmTMB','PBSmapping','colorRamps',
     'rstan','brms','Rcpp','ggpubr', 'rgdal','party', 'extrafont','viridis','english',
-    'janitor','bestglm')
+    'janitor','bestglm','DHARMa','MuMIn')
 
 update.packages(ask=F)
 
