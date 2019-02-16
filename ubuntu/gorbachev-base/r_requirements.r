8893d769d5a000ad950909c9824b0a92a2110490
markschambers <39928886+markschambers@users.noreply.github.com>
2019-02-16 13:04:55 +1300

    Add the arm package

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -15,7 +15,7 @@ packages <- c(
     'mgcv', 'geosphere', 'dbplyr', 'timeline', 'ggforce', 'CheckDigit', 'tinytex',
     'kableExtra', 'fuzzyjoin','lunar','gamlss','glmmTMB','PBSmapping','colorRamps',
     'rstan','brms','Rcpp','ggpubr', 'rgdal','party', 'extrafont','viridis','english',
-    'janitor','bestglm','DHARMa','MuMIn')
+    'janitor','bestglm','DHARMa','MuMIn','arm')
 
 update.packages(ask=F)
 
