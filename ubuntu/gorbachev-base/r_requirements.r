660302a5128999a41dd2862023b6d46f83215d41
markschambers <39928886+markschambers@users.noreply.github.com>
2019-09-05 09:40:46 +1200

    sampling package for stratified sampling

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -15,7 +15,7 @@ packages <- c(
     'mgcv', 'geosphere', 'dbplyr', 'timeline', 'ggforce', 'CheckDigit', 'tinytex',
     'kableExtra', 'fuzzyjoin','lunar','gamlss','glmmTMB','PBSmapping','colorRamps',
     'rstan','brms','Rcpp','ggpubr', 'rgdal','party', 'extrafont','viridis','english',
-    'janitor','bestglm','DHARMa','MuMIn','arm','ggrepel','truncdist','sf','fields','raster','glm2')
+    'janitor','bestglm','DHARMa','MuMIn','arm','ggrepel','truncdist','sf','fields','raster','glm2','sampling')
 
 update.packages(ask=F)
 
