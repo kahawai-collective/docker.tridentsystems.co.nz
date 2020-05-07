b862928f870806fdea119f041c5dcd0bda9b34b4
Laura Tremblay-Boyer <laura@dragonfly.co.nz>
2020-05-07 15:35:45 +1200

    setting CRAN repo to March snapshot

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -1,11 +1,13 @@
+# Set the URL for CRAN
 old <- getOption("defaultPackages"); r <- getOption("repos")
-r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
+#r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
+r['CRAN'] = 'https://mran.microsoft.com/snapshot/2020-03-25' 
 options(defaultPackages = c(old, "MASS"), repos = r, warn=2)
 
 # packages ‘grid’, ‘genoud’, ‘tools’, ‘utils’ are not available (for R version 3.4.2)
 
 packages <- c(
-    "shapefiles", "sp", "lattice", "RColorBrewer",
+    "shapefiles", "sp", "lattice", "RColorBrewer", "foreign",
     "maptools", "RPostgreSQL", "knitr", "rjson", "pander", "tidyverse",
     "tables", "data.table", "gridExtra", "rjags", "R2jags", "reshape2",
     "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'scales',
@@ -29,11 +31,10 @@ pkgs2install <- setdiff(packages, existing)
 message(sprintf('Installing packages: %s', paste(pkgs2install, collapse=', ')))
 
 
-old_foreign <- "https://cran.r-project.org/src/contrib/Archive/foreign/foreign_0.8-76.tar.gz"
-install.packages(old_foreign, repos=NULL, type="source")
+#old_foreign <- "https://cran.r-project.org/src/contrib/Archive/foreign/foreign_0.8-76.tar.gz"
+#install.packages(old_foreign, repos=NULL, type="source")
 
 install.packages(pkgs2install)
 
-## Start with older versions in case they are dependencies of the newer ones
 old_rgeos <- "https://cran.r-project.org/src/contrib/Archive/rgeos/rgeos_0.3-28.tar.gz"
 install.packages(old_rgeos, repos=NULL, type="source")
