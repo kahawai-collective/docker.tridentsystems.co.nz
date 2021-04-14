090fa5cb240e6fb01984322b99c9c21b05367c97
McKenzie Tornquist <mckenzie@dragonfly.com>
2021-04-14 12:18:18 +1200

    r date update

--- ubuntu/gorbachev-base-bleedingedge/r_requirements.r
+++ ubuntu/gorbachev-base-bleedingedge/r_requirements.r
@@ -1,7 +1,7 @@
 # Set the URL for CRAN
 old <- getOption("defaultPackages"); r <- getOption("repos")
 #r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
-r['CRAN'] = 'https://mran.microsoft.com/snapshot/2021-04-14'
+r['CRAN'] = 'https://mran.microsoft.com/snapshot/2021-04-13'
 options(defaultPackages = c(old, "MASS"), repos = r, warn=2)
 
 # packages ‘grid’, ‘genoud’, ‘tools’, ‘utils’ are not available (for R version 3.4.2)
