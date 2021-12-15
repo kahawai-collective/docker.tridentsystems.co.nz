6f14333c3f18c1b2ea4c5ce60cbb0b31266b6eb3
Philipp-Neubauer <neubauer.phil@gmail.com>
2021-12-15 15:03:48 +1300

    try update just stan docker

--- ubuntu/stan_2.26/r_requirements.r
+++ ubuntu/stan_2.26/r_requirements.r
@@ -2,10 +2,6 @@ old <- getOption("defaultPackages"); r <- getOption("repos")
 r["CRAN"] <- "http://cran.stat.auckland.ac.nz"
 options(defaultPackages = c(old, "MASS"), repos = r)
 
-remove.packages(c("StanHeaders","rstan",'RcppEigen','RcppParallel'))
+remotes::install_github("stan-dev/cmdstanr")
+install.packages("brms")
 
-options(repos = "https://cloud.r-project.org/")
-install.packages("remotes")
-remotes::install_git('https://github.com/RcppCore/RcppParallel')
-install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
