0bb8467bb8620b4aeae8740071af71dbfa13d12f
David Middleton <david@pisces.nz>
2023-01-25 08:51:59 +1300

    location

--- images/build.sh
+++ images/build.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-ls
-
-cp /input/mpi-latex-templates-local/mpi-latex-templates_1.54_all.deb .
+cp /input/mpi-latex-templates-local/mpi-latex-templates_1.54_all.deb ubuntu/gorbachev-base-jammy/.
 
 make deploy
 
