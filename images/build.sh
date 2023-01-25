090ed45da8b04dcbed7ddab996526a7ff97dff3f
David Middleton <david@pisces.nz>
2023-01-25 13:52:34 +1300

    Update MPI templates to 1.55

--- images/build.sh
+++ images/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cp /input/mpi-latex-templates-local/mpi-latex-templates_1.54_all.deb ubuntu/gorbachev-base-jammy/.
+cp /input/mpi-latex-templates-local/mpi-latex-templates_1.55_all.deb ubuntu/gorbachev-base-jammy/.
 
 make deploy
 
