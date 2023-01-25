61bfde9c5acfc42e25929cdde3484d6cbf7717af
David Middleton <david@pisces.nz>
2023-01-25 20:03:37 +1300

    rename

--- images/build.sh
+++ images/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cp /input/mpi-latex-templates-local/mpi-latex-templates_1.55_all.deb ubuntu/gorbachev-base-jammy/.
+cp /input/mpi-latex-templates-local/mpi-latex-templates_1.55_all.deb ubuntu/gorbachev-base-jammy/mpi-latex-templates_1.55.1_all.deb
 
 make deploy
 
