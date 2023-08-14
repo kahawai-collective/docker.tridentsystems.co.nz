4663650afb0cea8cdac7e6da2d20d2376add048a
David Middleton <david@pisces.nz>
2023-08-14 13:26:55 +0100

    gghighlight in quarto

---
+++ images/ubuntu/quarto/r_requirements.r
@@ -0,0 +1,23 @@
+
+install_loc <- "/usr/lib/R/site-library"
+
+# Set the URL for CRAN
+cran_nz <- "http://cran.stat.auckland.ac.nz"
+
+packages <- c(
+    'gghighlight'
+)
+
+
+existing <- tryCatch({
+    library()$results[, 'Package']
+}, error = function (x) {
+    c()
+})
+
+pkgs2install <- setdiff(packages, existing)
+message(sprintf('Installing packages: %s', paste(pkgs2install, collapse=', ')))
+install.packages(pkgs2install,
+                 lib = install_loc,
+                 repos = cran_nz)
+
