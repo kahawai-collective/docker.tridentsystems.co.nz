39284c4f5d7bc62e001fe639d745848696c16124
Teresa Amar <teresa@dragonfly.co.nz>
2024-01-11 11:46:06 +1300

    Make new docker image for paua stock assessments

---
+++ images/ubuntu/paua-jammy/r_requirements.r
@@ -0,0 +1,29 @@
+# packages for the paua stock assessments
+# and model and MPE diagnostics
+
+install_loc <- "/usr/lib/R/site-library"
+
+# Set the URL for CRAN
+cran_nz <- "http://cran.stat.auckland.ac.nz"
+
+packages <- c(
+    'hexbin',
+    'ggplot2',
+    'corpcor',
+    'flextable',
+    'equatags',
+    'reactable'
+)
+
+
+existing <- tryCatch({
+    library()$results[, 'Package']
+}, error = function (x) {
+    c()
+})
+
+pkgs2install <- setdiff(packages, existing)
+message(sprintf('Installing packages: %s', paste(pkgs2install, collapse=', ')))
+install.packages(pkgs2install,
+                 lib = install_loc,
+                 repos = c(cran_nz, getOption('repos')))
