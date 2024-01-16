317da936694f439a75b914f6e75e52854aeda57e
David Middleton <david@pisces.nz>
2024-01-17 12:46:57 +1300

    Update mpi templates in gorbachev-base-jammy
    Using gorbachev.io template build

--- images/build.sh
+++ images/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cp /input/mpi-latex-templates-local/mpi-latex-templates_1.55_all.deb ubuntu/gorbachev-base-jammy/mpi-latex-templates_1.55_all.deb
+#cp /input/mpi-latex-templates-local/mpi-latex-templates_1.55_all.deb ubuntu/gorbachev-base-jammy/mpi-latex-templates_1.55_all.deb
 
 make deploy
 
